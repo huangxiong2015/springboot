2e823027d3904d4c8a55918840f34b7d819a0ab4
unknown <huangxiong2015@163.com>
2017-06-05 17:22:17 +0800

    Spring Boot干货系列：（三）开发Web应用之JSP篇

--- demo/src/main/java/com/wanhui/contoller/Chapter2.java
+++ demo/src/main/java/com/wanhui/contoller/Chapter2.java
@@ -17,19 +17,27 @@ import com.wanhui.entity.LearnResouce;
 @Controller
 @RequestMapping("/learn")
 public class Chapter2 {
+//	@RequestMapping("")
+//	public ModelAndView index(){
+//		List<LearnResouce>  learnResouces = new ArrayList<>();
+//		//https://wenku.baidu.com/user/mydocs
+//		LearnResouce bean = new LearnResouce("第一章", "小熊", "http://www.oschina.net/");
+//		learnResouces.add(bean);
+//		bean = new LearnResouce("第二章", "小辉", "http://www.weibo.com/?c=spr_sinamkt_buy_hyww_weibo_p137");
+//		learnResouces.add(bean);
+//		bean = new LearnResouce("第三章", "小培", "https://www.baidu.com/");
+//		learnResouces.add(bean);
+//		//引入依赖后就在默认的模板路径src/main/resources/templates下编写模板文件即可完成
+//		ModelAndView mv = new ModelAndView("/index");
+//		mv.addObject("learnResouces", learnResouces);
+//		return mv;
+//	}
+	
 	@RequestMapping("")
-	public ModelAndView index(){
-		List<LearnResouce>  learnResouces = new ArrayList<>();
-		//https://wenku.baidu.com/user/mydocs
-		LearnResouce bean = new LearnResouce("第一章", "小熊", "http://www.oschina.net/");
-		learnResouces.add(bean);
-		bean = new LearnResouce("第二章", "小辉", "http://www.weibo.com/?c=spr_sinamkt_buy_hyww_weibo_p137");
-		learnResouces.add(bean);
-		bean = new LearnResouce("第三章", "小培", "https://www.baidu.com/");
-		learnResouces.add(bean);
-		//引入依赖后就在默认的模板路径src/main/resources/templates下编写模板文件即可完成
-		ModelAndView mv = new ModelAndView("/index");
-		mv.addObject("learnResouces", learnResouces);
-		return mv;
+	public String Chapter3() {
+		return "/cc";
 	}
 }
+
+
+
